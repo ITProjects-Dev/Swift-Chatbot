67f804ebbe04c9ff4a51019b73ab4963dd085be2
Adithiinchure <adithiinchurai@gmail.com>
2026-06-24 16:52:05 +0530

    added

--- database.py
+++ database.py
@@ -1,14 +1,17 @@
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 import os
 
 load_dotenv()
 
+password = quote_plus(os.getenv("DB_PASSWORD"))
+
 DATABASE_URL = (
     f"mysql+pymysql://"
     f"{os.getenv('DB_USER')}:"
-    f"{os.getenv('DB_PASSWORD')}"
-    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+    f"{password}"
+    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT', '3306')}/{os.getenv('DB_NAME')}"
 )
 
 engine = create_engine(DATABASE_URL)
